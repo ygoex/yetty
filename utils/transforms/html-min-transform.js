da809411749cc41a31835901834fbf977b9182f4
Yolanda Goex <helloygoex@gmail.com>
2021-03-24 15:24:37 +0000

    Check if we are in prod before minifying.

--- utils/transforms/html-min-transform.js
+++ utils/transforms/html-min-transform.js
@@ -1,12 +1,12 @@
 const htmlmin = require("html-minifier");
 
 module.exports = function htmlMinTransform(content, outputPath) {
-  // bail if not production env
+  // If not production
   if (process.env.ELEVENTY_ENV !== 'production') {
     return content
   }
   // returned minified content from html files
-  if (outputPath.endsWith('.html')) {
+  if (process.env.ELEVENTY_ENV === 'production' && outputPath.endsWith('.html')) {
     let minified = htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
