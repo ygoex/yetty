0fc37600fc3dd8e27a38da3db8fb54ec5ca3c430
Yolanda Goex <helloygoex@gmail.com>
2021-03-28 09:59:08 +0100

    Delete transpiler

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,8 @@ const path = require('path');
 
 module.exports = {
   entry: ['./src/assets/scripts/index.js'],
-  target: 'web',
   output: {
     path: path.resolve(__dirname, 'src/assets/scripts'),
-    filename: 'main.js'
+    filename: 'bundle.min.js'
   },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
-  }
 }
