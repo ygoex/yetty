b34eb4d2504ec0a608449d1e33a4af9b9fe8ffe0
Yolanda Goex <helloygoex@gmail.com>
2021-04-09 21:58:53 +0100

    Remove local-respimg plugin.

--- .eleventy.js
+++ .eleventy.js
@@ -10,7 +10,6 @@ const CleanCSS = require("clean-css");
 const criticalCss = require("eleventy-critical-css");
 const pluginPWA = require("eleventy-plugin-pwa");
 const imageShortcode = require('./utils/shortcodes/imageProcess.js');
-const pluginLocalRespimg = require('eleventy-plugin-local-respimg');
 
 // Import transforms
 const htmlMinTransform = require("./utils/transforms/html-min-transform.js");
@@ -41,46 +40,6 @@ module.exports = function(eleventyConfig) {
   }
   // PWA
   eleventyConfig.addPlugin(pluginPWA);
-
-  // Responsive images in markdown
-  eleventyConfig.addPlugin(pluginLocalRespimg, {
-    folders: {
-      source: './src', // Folder images are stored in
-      output: './dist', // Folder images should be output to
-    },
-    images: {
-      resize: {
-        min: 480, // Minimum width to resize an image to
-        max: 1440, // Maximum width to resize an image to
-        step: 480, // Width difference between each resized image
-      },
-      hoistClasses: false, // Adds the image tag's classes to the output picture tag
-      gifToVideo: false, // Convert GIFs to MP4 videos
-      sizes: '100vw', // Default image `sizes` attribute
-      lazy: true, // Include `loading="lazy"` attribute for images
-      watch: {
-        src: 'assets/images/**/*', // Glob of images that Eleventy should watch for changes to
-      },
-      pngquant: {
-        /* ... */
-      }, // imagemin-pngquant options
-      mozjpeg: {
-        /* ... */
-      }, // imagemin-mozjpeg options
-      svgo: {
-        /* ... */
-      }, // imagemin-svgo options
-      gifresize: {
-        /* ... */
-      }, // @gumlet/gif-resize options
-      webp: {
-        quality: 50,
-      }, // imagemin-webp options
-      gifwebp: {
-        /* ... */
-      }, // imagemin-gif2webp options
-    },
-  });
 
   // Setup mermaid markdown highlighter
   const highlighter = eleventyConfig.markdownHighlighter;
