403eb860c0e8fb4488ce742fbeb6da525cca2ff5
Yolanda Goex <helloygoex@gmail.com>
2021-04-19 13:43:03 +0100

    Add new arguments to image function.

--- utils/shortcodes/imageProcess.js
+++ utils/shortcodes/imageProcess.js
@@ -3,12 +3,11 @@ const Image = require("@11ty/eleventy-img");
 const developmentFormats = ["jpeg"];
 const productionFormats = ["avif", "webp", "jpeg"];
 
-async function imageShortcode(src, alt, sizes = "100vw") {
+async function imageShortcode(src, alt, sizes = '100vw', pictureClass, cssClass, bannerBorderColor = 'transparent') {
   if(alt === undefined) {
-    // You bet we throw an error on missing alt (alt="" works okay)
-    throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
+    // We throw an error on missing alt (alt="" works okay for decorative images)
+    throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`)
   }
-
   let metadata = await Image(src, {
     widths: [480, 960, 1440],
     /**
@@ -16,11 +15,11 @@ async function imageShortcode(src, alt, sizes = "100vw") {
      * so let's skip all that image processing in development.
      */
     formats:
-      process.env.ELEVENTY_ENV === "production"
-        ? productionFormats
-        : developmentFormats,
+    process.env.ELEVENTY_ENV === "production"
+      ? productionFormats
+      : developmentFormats,
     /**
-     * A path-prefix-esque directory for the <img src> attribute.
+     * A path-prefix-esque directory for the <img src> attribute
      * e.g. /img/ for <img src="/img/MY_IMAGE.jpeg">
      */
     urlPath: "/assets/images/",
@@ -31,34 +30,32 @@ async function imageShortcode(src, alt, sizes = "100vw") {
      */
     outputDir: "./dist/assets/images/",
     filenameFormat: function (id, src, width, format, options) {
-      // id: hash of the original image
-      // src: original image path
-      // width: current width in px
-      // format: current file format
-      // options: set of options passed to the Image call
-      const extension = path.extname(src);
-      const name = path.basename(src, extension);
-      return `${name}-${width}.${format}`;
-    }
-  });
-
+    // id: hash of the original image
+    // src: original image path
+    // width: current width in px
+    // format: current file format
+    // options: set of options passed to the Image call
+    const extension = path.extname(src);
+    const name = path.basename(src, extension);
+    return `${name}-${width}.${format}`;
+  }
+  })
   //Take the smaller image to be used in the img tag
   let lowsrc = metadata.jpeg[0];
 
-  return `<picture>
-    ${Object.values(metadata).map(imageFormat => {
-      return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`;
-    }).join("\n")}
-      <img
-        class="img-tmpl"
-        src="${lowsrc.url}"
-        width="${lowsrc.width}"
-        height="${lowsrc.height}"
-        alt="${alt}"
-        loading="lazy"
-        decoding="async">
-    </picture>`;
-
-};
+  return `<picture class="${pictureClass}" style="--banner-border-color: ${bannerBorderColor}">
+  ${Object.values(metadata).map(imageFormat => {
+    return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`;
+  }).join("\n")}
+    <img
+      class="${cssClass}"
+      src="${lowsrc.url}"
+      width="${lowsrc.width}"
+      height="${lowsrc.height}"
+      alt="${alt}"
+      loading="lazy"
+      decoding="async">
+  </picture>`;
+}
 
 module.exports = imageShortcode;
