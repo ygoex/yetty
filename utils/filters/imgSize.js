a152dc79032630b5e1b2f79f114fb81ebd30f7a4
Yolanda Goex <helloygoex@gmail.com>
2021-05-12 20:15:20 +0100

    Create new filter to include thumbnail or small images in cards, etc.

---
+++ utils/filters/imgSize.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = function imgSize(src, size = '100', format = 'jpeg') {
+  const extension = path.extname(src);
+  const name = path.basename(src, extension);
+  /**
+   * A path directory for the <img src> attribute
+   * e.g. /img/ for <img src="/img/MY_IMAGE.jpeg">
+   */
+  const dirname = "/assets/images/";
+  return `${dirname}${name}-${size}.${format}`;
+};
